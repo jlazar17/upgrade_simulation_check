9db8e23007535dbbfea1fb03b360ca49aca38be4
jlazar17 <jeff.p.lazar@gmail.com>
2024-03-24 11:28:49 -0500

    add DEgg reference position

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,8 @@
 export DATADIR=$PWD/../data/
+export DATAPREFIX="upgrade_checks_"
+
+export PYTHONPATH=$PYTHONPATH:$PWD/..
+export GCDFILE="/data/sim/IceCubeUpgrade/genie/step3/140029/GeoCalibDetectorStatus_ICUpgrade.v58.mixed.V1.i3.bz2"
+export DEGGREFX="14.29" # x-position of one of the DEggs that I found in `$GCDFILE`
+export DEGGREFY="-80.5637" # y-position of one of the DEggs that I found in `$GCDFILE`
+export DEGGREFZ="-601.93" # z-position of one of the DEggs that I found in `$GCDFILE`
