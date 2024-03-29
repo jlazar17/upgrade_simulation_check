46eb7d98ffcc1f110e043da95a9d00e46ff5d45d
jlazar17 <jeff.p.lazar@gmail.com>
2024-03-29 04:46:00 -0500

    Allow source from anywhere

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,5 @@
-export DATADIR=`realpath $PWD/../data/`
+SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export DATADIR=`realpath $SCRIPTDIR/../data/`
 export DATAPREFIX="upgrade_checks_"
 
 export PYTHONPATH=$PYTHONPATH:$PWD/..
@@ -9,6 +10,8 @@ export GCDFILESTEP2="/data/sim/IceCubeUpgrade/geometries/GCDs/GeoCalibDetectorSt
 export ANGSENS="as.flasher_p1_0.30_p2_-1"
 export ICEMODEL="spice_bfr-v2"
 export OVERSIZE=3
+export GLASS=vitrovex
+export EFFICIENCY=1.2
 
 export DEGGREFX="14.29" # x-position of one of the DEggs that I found in `$GCDFILE`
 export DEGGREFY="-80.5637" # y-position of one of the DEggs that I found in `$GCDFILE`
