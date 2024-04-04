d29f18d8ac10d4ea04433c26e577cc7e9ccc15e3
jlazar17 <jeff.p.lazar@gmail.com>
2024-04-04 14:02:04 -0500

    Update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,11 @@ SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export DATADIR=`realpath $SCRIPTDIR/../data/`
 export DATAPREFIX="upgrade_checks_"
 
+export UPGRADEBUILDDIR=/data/user/jlazar/icetrays/Upgrade/build/
+export UPGRADESRCDIR=/data/user/jlazar/icetrays/Upgrade/src/
+export GEN2BUILDDIR=/data/user/jlazar/icetrays/Gen2Sim/build/
+export GEN2SRCDIR=/data/user/jlazar/icetrays/Gen2Sim/src/
+
 export PYTHONPATH=$PYTHONPATH:$PWD/..
 
 export GCDFILE="/data/sim/IceCubeUpgrade/geometries/GCDs/GeoCalibDetectorStatus_ICUpgrade.v58.mixed.V1.i3.bz2"
@@ -11,7 +16,8 @@ export ANGSENS="as.flasher_p1_0.30_p2_-1"
 export ICEMODEL="spice_bfr-v2"
 export OVERSIZE=1 # CLSim will blow a gasket when converting to MCPE if this isn't 1
 export GLASS=vitrovex
-export EFFICIENCY=1.2
+export MAXEFFICIENCY=1.2
+export NOMINALEFFICIENCY=1.0
 
 export DEGGREFX="14.29" # x-position of one of the DEggs that I found in `$GCDFILE`
 export DEGGREFY="-80.5637" # y-position of one of the DEggs that I found in `$GCDFILE`
