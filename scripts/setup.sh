8f742bf56ed872de2c4c0d9613718807d35e661c
jlazar17 <jeff.p.lazar@gmail.com>
2024-04-09 16:41:49 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,12 +7,12 @@ export UPGRADESRCDIR=/data/user/jlazar/icetrays/Upgrade/src/
 export GEN2BUILDDIR=/data/user/jlazar/icetrays/Gen2Sim/build/
 export GEN2SRCDIR=/data/user/jlazar/icetrays/Gen2Sim/src/
 
-export PYTHONPATH=$PYTHONPATH:$PWD/..
+export PYTHONPATH=$PYTHONPATH:$SCRIPTDIR/..
 
 export GCDFILE="/data/sim/IceCubeUpgrade/geometries/GCDs/GeoCalibDetectorStatus_ICUpgrade.v58.mixed.V1.i3.bz2"
 export GCDFILESTEP2="/data/sim/IceCubeUpgrade/geometries/GCDs/GeoCalibDetectorStatus_ICUpgrade.v58.sDOM.V1.i3.bz2"
 
-export ANGSENS="as.flasher_p1_0.30_p2_-1"
+export ANGSENS="as.nominal"
 export ICEMODEL="spice_bfr-v2"
 export OVERSIZE=1 # CLSim will blow a gasket when converting to MCPE if this isn't 1
 export GLASS=vitrovex
